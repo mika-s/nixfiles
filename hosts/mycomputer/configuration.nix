aec5fa12662fc21abe279a176b5927caf49aa46c
Mika Sundland <mika.sundland@gmail.com>
2023-08-27 01:12:46 +0200

    Format and remove unnecessary stuff from configuration.nix

--- hosts/mycomputer/configuration.nix
+++ hosts/mycomputer/configuration.nix
@@ -2,7 +2,7 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
       <home-manager/nixos>
     ];
@@ -45,7 +45,6 @@
     font = "Lat2-Terminus16";
     useXkbConfig = true;
   };
-  # console.keyMap = "no";
 
   services.xserver = {
     enable = true;
@@ -94,8 +93,8 @@
     git
     htop
     libreoffice-qt
-    kate
     libsForQt5.kcalc
+    kate
     keepassxc
     jetbrains.clion
     nextcloud-client
@@ -121,52 +120,21 @@
     extraRules = [{
       commands = [
         {
-	  command = "${pkgs.systemd}/bin/systemctl suspend";
-	  options = [ "NOPASSWD" ];
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+          options = [ "NOPASSWD" ];
         }
         {
-	  command = "${pkgs.systemd}/bin/reboot";
-	  options = [ "NOPASSWD" ];
-	}
-	{
-	  command = "${pkgs.systemd}/bin/poweroff";
-	  options = [ "NOPASSWD" ];
-	}
+          command = "${pkgs.systemd}/bin/reboot";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.systemd}/bin/poweroff";
+          options = [ "NOPASSWD" ];
+        }
       ];
       groups = [ "wheel" ];
     }];
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
+  system.stateVersion = "23.05";
 }
